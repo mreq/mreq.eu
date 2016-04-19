696266de4c26f6b1a94d944a0d62f4a52658a37a
Petr Marek <contact@petrmarek.eu>
2016-04-19 23:06:51 +0200

    cleanup

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sh generate_thumbnails.sh
-middleman build
-rm -rf build/tagged
-rsync -avz /home/petr/work/mreq-blog/build/ mreq:/home/petr/php/mreq
-	
-notify-send -u low -i emblem-default 'Build done.' 'Changes rsynced.'
